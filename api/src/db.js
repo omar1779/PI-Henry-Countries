202f2e38f63ab8932ad9c1ffa82e778995c1534d
omar1779 <josemyclans@gmail.com>
2022-07-17 02:27:18 -0500

    modelado y relacion de DB

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { country , activity } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+// Countries.hasMany(activity);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
